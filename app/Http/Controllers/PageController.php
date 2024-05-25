c993a1410c54a6b57414d480a0a8b0da8742e381
ClaudioPerrone <cla-agadir97@hotmail.it>
2024-05-25 18:15:38 +0200

    Feat: added controller, model and printed on screen the movies

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function index() {
-        dd('ciao ciao');
+        //dd('ciao ciao');
+        $movies = Movie::all();
+        //dd($movies);
+
+        $data = [
+            'movies' => $movies
+        ];
+
+        return view('home', $data);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,20 @@
 
 @section('main-content')
     <h1>Home</h1>
+
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="col-2">
+                    <div class="card p-3">
+                        <h3 class="text-center">Title: {{ $movie['title'] }}</h3>
+                        <h5 class="text-center">OG title: {{ $movie['original_title'] }}</h5>
+                        <h5 class="text-center">Nationality: {{ $movie['nationality'] }}</h5>
+                        <h5 class="text-center">Date: {{ $movie['date'] }}</h5>
+                        <h5 class="text-center">Vote: {{ $movie['vote'] }}</h5>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
